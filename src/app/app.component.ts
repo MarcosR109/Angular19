f1c7bcfaff334999cce687effc232a87a503eadb
MarcosR109 <156894417+MarcosR109@users.noreply.github.com>
2025-01-14 11:10:47 +0000

    ngfor completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,44 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  /**
+   *
+   */
+  constructor() {
+    this.libros=[
+      {id:1,titulo:'Harry Potter',autor:'JK Rowling'},
+      {id:2,titulo:'El señor de los anillos',autor:'Tolkien'},
+      {id:3,titulo:"Juramentada",autor:'Brandon Sanderson'}
+    ]
+    this.libros1=[
+      {id:1,titulo:'Harry Potter',autor:'JK Rowling'},
+      {id:2,titulo:'El señor de los anillos',autor:'Tolkien'},
+      {id:3,titulo:"Juramentada",autor:'Brandon Sanderson'}
+    ]
+  }
+
   title = 'Hola!';
   condicion:Boolean = true;
   dias_semana:string="";
   vbleColor:string = 'red';
   color:string = 'yellow';
   colorFondo:string = 'purple';
+  libros:Array<any>;
+  libros1:Array<Libro>;
+  showAuthor(libro:Libro){
+    const verAuthor = "Escrito por " + libro.autor;
+    alert (verAuthor);
+    console.log(libro.titulo + " escrito por " + libro.autor);
+  }
 }
+interface Libro{
+  id:Number;
+  titulo:String;
+  autor:string;
+}
+function showAuthor(libro:Libro){
+  const verAuthor = "Escrito por " + libro.autor;
+  alert (verAuthor);
+  console.log(libro.titulo + " escrito por " + libro.autor);
+}
+
