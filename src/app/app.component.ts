ed3675338709ae22240dfbc73dd7da17917542d4
MarcosR109 <156894417+MarcosR109@users.noreply.github.com>
2025-01-14 08:27:33 +0000

    Empezar ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { PruebasComponent } from './pruebas/pruebas.component';
 import { ColoresComponent } from './colores/colores.component';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PruebasComponent,ColoresComponent],
+  imports: [RouterOutlet, PruebasComponent,ColoresComponent,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Hola!';
+  condicion:Boolean = true;
+  dias_semana:string="";
+  vbleColor:string = 'red';
+  color:string = 'yellow';
+  colorFondo:string = 'purple';
 }
